70e435d9228421da80a717e57c289f1096b5833a
lucio dijasio <lucio.dijasio@googlemail.com>
2014-04-02 12:01:17 +0200

    Replaced Animate list to avoid using list (pointers) Reduced card(s) cases single/multiple Added Undo of refill and re-hide of cards

--- solitaire.py
+++ solitaire.py
@@ -10,50 +10,48 @@ import pygame, sys
 # Controller
 #
 
-def animate( dlist, dXY, slist, index, sXY):
-    global Adest, Log
+def animate( dest, dindex, dXY, source, sindex, slen, sXY):
+    global Adest, Aindex, Log
 
     # program animation
     dx, dy = dXY     # get the tip dest coordinates
     sx, sy = sXY     # get source coordinates
-    Adest = dlist    # list to append to after animation
+    
+    # save the destination list
+    Adest = dest    # list to append to after animation
+    Aindex = dindex
 
     # remove card/tail from source list
-    if index == -1:     # single card (tip)
-        card = slist.pop()
-        # log the move
-        Log.append( ( dlist, -1, dXY, slist, sXY))
-
-    else:
-        card = slist[ index:]
-        Log.append( ( dlist, len( dlist), dXY, slist, sXY))
-        while len(slist) > index:
-            slist.pop()
-
+    cards = model.getCards( source, sindex, slen)
+    # log the move
+    Log.append( ("move", (dest, dindex, dXY, source, sindex, slen, sXY)))
 
     # final point
-    Asteps.append( ( card, 0, 0)) # only one step, final
+    Asteps.append( ( cards, 0, 0)) # only one step, final
     # 3/4 point
-    Asteps.append( ( card, sx+3*(dx-sx)/4, sy+3*(dy-sy)/4))
+    Asteps.append( ( cards, sx+3*(dx-sx)/4, sy+3*(dy-sy)/4))
     # half point
-    Asteps.append( ( card, sx+2*(dx-sx)/4, sy+2*(dy-sy)/4))
+    Asteps.append( ( cards, sx+2*(dx-sx)/4, sy+2*(dy-sy)/4))
     # 1/4 point
-    Asteps.append( ( card, sx+1*(dx-sx)/4, sy+1*(dy-sy)/4))
+    Asteps.append( ( cards, sx+1*(dx-sx)/4, sy+1*(dy-sy)/4))
 
 
 def undo():
     global Log
     if Log:
         # animate the reverse of the last move
-        dlist, index, dXY, slist, sXY = Log.pop()
-        animate( slist, sXY, dlist, index, dXY)
-        Log.pop()   # avoid logging the undo action
+        action, x  = Log.pop()
+        if action == "move":
+            dest, dindex, dXY, source, sindex, slen, sXY = x
+            animate( source, sindex, sXY, dest, dindex, slen, dXY)
+            Log.pop()   # avoid logging the undo action
 
-        # todo
-        # 1. unhide
-        # 2. refill stack detection
+        elif action == "unhide":
+            undo()
+            model.rowHide( x)
 
-        pass
+        elif action == "refill":
+            model.unrefillStack()
 
 
 if __name__ == "__main__":
@@ -79,6 +77,7 @@ if __name__ == "__main__":
 
     Asteps = []      # list of pre-computed steps for animation
     Adest = None     # target list for card at the end of animation
+    Aindex = None    
 
     # main loop
     while( True):
@@ -92,43 +91,38 @@ if __name__ == "__main__":
                 touch = view.checkTouch ( pygame.mouse.get_pos())
 
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_m:
+                if event.key == pygame.K_q:
+                    sys.exit()
+
+                elif event.key == pygame.K_m:
                     touch = ("basket", 0, (view.basket_x, view.basket_y))
 
                 elif event.key == pygame.K_SPACE:
                     touch = ("stack", 0, (view.stack_x, view.stack_y))
 
+                elif event.key == pygame.K_COMMA:
+                    touch = ("button", 0, 0)
 
         # check if animation pending
         if Asteps:
-            step = Asteps.pop()
-            if Asteps:
+            cards, x, y = Asteps.pop()
+            if Asteps: # there are more steps pending
                 # update the view and then add the card in motion
                 view.display()
-                # check if passed a tail (list of cards)
-                if isinstance( step[0], list):
-                    x, y = step[1], step[2]
-                    for card in step[0]:
-                        if card == step[0][-1]: # last in the tail
-                            view.printFullCard( card, x, y)
-                        else:
-                            view.printTopCard( card, x, y)
-                        y += view.cardtop_h
+                for card in cards:
+                    if card == cards[-1]: # last in the tail
+                        view.printFullCard( card, x, y)
+                    else:
+                        view.printTopCard( card, x, y)
+                    y += view.cardtop_h
 
-                else:   #single card
-                    view.printFullCard( *step)
-
-            else:   # end of animation append/extend
-                # place card in final position then update view
-                if isinstance( step[0], list):
-                    Adest.extend( step[0])
-                else: # single card append
-                    Adest.append( step[0])
-                # and record the move done
+            else:   # end of animation 
+                # place card(s) in final position then update view
+                cards.reverse()
+                for card in cards:
+                    model.moveTo( Adest, Aindex, card)
                 model.countSteps += 1
-                # only now update the view
                 view.display()
-
 
         # victory?
         elif Victory:
@@ -147,16 +141,16 @@ if __name__ == "__main__":
 
         # auto mode activated
         elif Auto:
-            # try to use the last card in the basket
+            # try to use the top card in the basket
             if model.basket:
                 card = model.getBasketTip()
                 # try to move the card automatically to the first matching row
                 top = model.matchTop( card)
                 if top >= 0:
-                    animate( model.tops[ top],      # dest list
-                             view.topXY[ top],      # dest coord
-                             model.basket,-1,       # source list
-                             (view.basket_x, view.basket_y))  # source coords
+                    animate( 't', top,                      # dest list
+                             view.topXY[ top],              # dest coord
+                             'b', 0, 1,                     # source list
+                             (view.basket_x, view.basket_y))# source coords
                     Victory = model.checkFinished()
                     # update display
                     view.display()
@@ -169,9 +163,9 @@ if __name__ == "__main__":
                     # check if it can go to the top
                     top = model.matchTop( card)
                     if top >= 0:
-                        animate( model.tops[ top],          # dest list
+                        animate( 't', top,                  # dest list
                                  view.topXY[ top],          # dest coord
-                                 model.rows[ i], -1,        # source list
+                                 'r', i, 1,                 # source list
                                  view.tipXY[ i])            # source coords
                         Victory = model.checkFinished()
                         # update display
@@ -179,112 +173,111 @@ if __name__ == "__main__":
                         break
 
 
-
         # else respond to last user input
         elif touch:
+            obj, srow, xy = touch
 
-            if touch[0] == "stack":
-                card = model.getStackTip()
-                # if stack is empty
-                if not card:
-                    model.refillStack()
-                else:   # get the top card out on the basket
-                    #model.dropInBasket( st)
-                    animate( model.basket,                  # dest list
+            if obj == "stack":
+                if model.stack:
+                    # get the top card down into the basket
+                    animate( 'b', 0,                        # dest list
                              (view.basket_x, view.basket_y),# dest coord
-                             model.stack, -1,               # source list
-                             touch[2])                      # source coord
-
-            elif touch[0] == "tip":
-                card = model.getTip( touch[1])
+                             's', 0, 1,                     # source list
+                             xy)                            # source coord
+                else: 
+                    # if stack is empty
+                    if model.basket:    # and basket is not empty
+                        model.refillStack()
+                        Log.append( ("refill", 0))
+                    
+            elif obj == "tip":
+                card = model.getTip( srow)
 
                 # check if it can go to the top
                 top = model.matchTop( card)
                 if top >= 0:
-                    animate( model.tops[ top],          # dest list
+                    animate( 't', top,                  # dest list
                              view.topXY[ top],          # dest coord
-                             model.rows[ touch[ 1]], -1,# source list
-                             touch[2])                  # source coords
-                    model.checkHidden( touch[ 1])
+                             'r',  srow, 1,             # source list
+                             xy)                        # source coords
+                    if model.checkHidden( srow):
+                        Log.append( ("unhide", srow))
 
                 # or to another row
                 else:
-                    row = model.matchRow( card)
-                    if row >= 0:
-                        animate( model.rows[ row],      # dest list
-                                 view.tipXY[ row],      # dest coord
-                                 model.rows[ touch[ 1]],# source list
-                                 -1,
-                                 touch[2])              # source coords
-                        model.checkHidden( touch[ 1])
+                    drow = model.matchRow( card)
+                    if drow >= 0:
+                        animate( 'r', drow,             # dest list
+                                 view.tipXY[ drow],     # dest coord
+                                 'r', srow, 1,          # source list
+                                 xy)                    # source coords
+                        if model.checkHidden( srow):
+                            Log.append( ("unhide", srow))
 
-
-            elif touch[0] == "tail":
-                row = touch[1]/16
+            elif obj == "tail":
+                srow = touch[1]/16
                 index = touch[1]%16
-                card = model.rows[ row][index]
+                card = model.rows[ srow][index]
                 drow = model.matchRow( card)
                 if drow >= 0:
-                    animate( model.rows[ drow],         # dest list
+                    slen = len( model.rows[srow]) - index
+                    animate( 'r', drow,                 # dest list
                              view.tipXY[ drow],         # dest coord
-                             model.rows[ row],          # source list
-                             index,
-                             touch[2])                  # source coord
-                    model.checkHidden( row)
+                             'r', srow, slen,           # source list
+                             xy)                        # source coord
+                    if model.checkHidden( srow):
+                        Log.append( ("unhide", srow))
 
-
-            elif touch[0] == "top":
-                card = model.getTop( touch[1])
+            elif obj == "top":
+                top = touch[1]
+                card = model.getTop( top)
                 row = model.matchRow( card)
                 if row >= 0:
-                    animate( model.rows[ row],          # dest list
+                    animate( 'r', row,                  # dest list
                              view.tipXY[ row],          # dest coord
-                             model.tops[ touch[ 1]],-1, # source list
-                             touch[2])                  # source coords
+                             't', top, 1,               # source list
+                             xy)                        # source coords
 
-            elif touch[0] == "basket":
+            elif obj == "basket":
                 card =  model.getBasketTip()
                 if card:
                     # try to move the card automatically to a top
                     top = model.matchTop( card)
                     if top >= 0:
-                        animate( model.tops[ top], # dest list
-                                 view.topXY[ top], # dest coord
-                                 model.basket,-1,  # source list
-                                 touch[2])         # source coords
+                        animate( 't', top,              # dest list
+                                 view.topXY[ top],      # dest coord
+                                 'b', 0, 1,             # source list
+                                 xy)                    # source coords
 
                     else: # move to a tip
                         row = model.matchRow( card)
                         if row >= 0:
-                            animate( model.rows[ row], # destination list
-                                     view.tipXY[ row], # dest coordinates
-                                     model.basket,-1,  # source list
-                                     touch[2])         # source coordinates
+                            animate( 'r', row,          # destination list
+                                     view.tipXY[ row],  # dest coordinates
+                                     'b', 0, 1,         # source list
+                                     xy)                # source coordinates
 
-            elif touch[0] == "button":
-                if   touch[ 1] == 0:  # undo
+            elif obj == "button":
+                if   srow == 0:  # undo
                     undo()
 
-                elif touch[ 1] == 1:  # restart
+                elif srow == 1:  # restart
                     model.restart()
                     model.countSteps = 0
                     Log=[]
 
-                elif touch[ 1] == 2:  # re-shuffle and deal a new one
+                elif srow == 2:  # re-shuffle and deal a new one
                     model.init()
                     model.shuffle()
                     model.deal()
                     model.countSteps = 0
                     Log=[]
 
-
             # update display
             view.display()
             # check if auto can be activated
             if not Auto:
-                Auto = (( model.countHidden() < 1) and 
-                            (not model.stack) and (not model.basket))
-
+                Auto = ( model.countHidden() < 1)
 
         # refresh
         view.wait( 20)
